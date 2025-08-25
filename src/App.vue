1636b3e33348dad041c8145996aa70a399c93431
tearill <1105317666@qq.com>
2025-08-25 14:26:50 +0800

    feat: init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page-wrapper">
+    <div class="content"></div>
+    <footer>
+      <a href=""></a>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+
+<style scoped></style>
